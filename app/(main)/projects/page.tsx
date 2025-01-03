a5761f90c95d87661a4c13b3344e01a18cc4c3c0
xuf <sherlock.xpf@gmail.com>
2025-01-03 16:15:49 +0800

    up:metadata

---
+++ app/(main)/projects/page.tsx
@@ -0,0 +1,41 @@
+import { type Metadata } from 'next'
+
+import { Projects } from '~/app/(main)/projects/Projects'
+import { Container } from '~/components/ui/Container'
+
+const title = '我的项目'
+const description =
+  '多年来，我一直在做各种各样的小项目，这里就是我筛选出来我觉得还不错的项目合集，也是我在技术领域中尝试和探索的最好见证。'
+export const metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+  },
+  twitter: {
+    title,
+    description,
+    card: 'summary_large_image',
+  },
+} satisfies Metadata
+
+export default function ProjectsPage() {
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <header className="max-w-2xl">
+        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+          关于做过的项目
+        </h1>
+        <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+          以下是我项目的集合，包括<b>海外交付项目、数字政务项目以及参与的开源项目</b>，还在不在探索。
+        </p>
+      </header>
+      <div className="mt-16 sm:mt-20">
+        <Projects />
+      </div>
+    </Container>
+  )
+}
+
+export const revalidate = 3600
