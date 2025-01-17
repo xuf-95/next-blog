921cca3fea5cc7ca8ea218330da0ddc4b344310c
xuf <sherlock.xpf@gmail.com>
2025-01-17 09:44:30 +0800

    cancel:guestbook

--- config/nav.ts
+++ config/nav.ts
@@ -1,8 +1,8 @@
 export const navigationItems = [
   { href: '/', text: 'Home' },
-  { href: '/blog', text: 'Blog' },
+  { href: '/blog', text: 'Blogs' },
   { href: '/projects', text: 'Projects' },
-  { href: '/guestbook', text: 'GuestBook' },
+  // { href: '/guestbook', text: 'GuestBook' },
   // { href: '/ama', text: 'AMA 咨询' },
-  //{ href: '/about', text: 'About' },
+  { href: '/about', text: 'About' },
 ]
