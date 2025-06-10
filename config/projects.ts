0eb69bf89fbadb910b50428b9658c1d6325c4f2a
xufei-0217 <sherloc.xpf@gmail.com>
2025-06-10 17:14:44 +0800

    up:skill

--- config/projects.ts
+++ config/projects.ts
@@ -1,49 +1,50 @@
-// projects
-export const projectHeadLine = "What I've done and what I'm doing."
-export const projectIntro = "I've worked on a variety of projects, from simple websites to complex web applications. And many of them are open-source. Here are a few of my favorites."
+// // projects
+// // reference: https://coreychiu.com/
+// export const projectHeadLine = "What I've done and what I'm doing."
+// export const projectIntro = "I've worked on a variety of projects, from simple websites to complex web applications. And many of them are open-source. Here are a few of my favorites."
 
-export type ProjectItemType = {
-    name: string
-    description: string
-    link: { href: string, label: string }
-    date?: string
-    logo?: string,
-    category?: string[],
-    tags?: string[],
-    image?: string,
-    techStack?: string[],
-    gitStars?: number,
-    gitForks?: number
-  }
+// export type ProjectItemType = {
+//     name: string
+//     description: string
+//     link: { href: string, label: string }
+//     date?: string
+//     logo?: string,
+//     category?: string[],
+//     tags?: string[],
+//     image?: string,
+//     techStack?: string[],
+//     gitStars?: number,
+//     gitForks?: number
+//   }
   
-  // projects 
-  export const projects: Array<ProjectItemType> = [
-    {
-      name: 'GitHub Cards',
-      description:
-        'Showcase your GitHub contributions into stunning visual cards.',
-      link: { href: 'github.cards', label: 'GitHub Cards' },
-      category: ['Website'],
-      techStack: ['Next.js', 'TailwindCSS', 'Shadcn/UI'],
-      tags: ['Visual Cards', 'GitHub Contribution Cards']
-    },
-  ]
+//   // projects 
+//   export const projects: Array<ProjectItemType> = [
+//     {
+//       name: 'GitHub Cards',
+//       description:
+//         'Showcase your GitHub contributions into stunning visual cards.',
+//       link: { href: 'github.cards', label: 'GitHub Cards' },
+//       category: ['Website'],
+//       techStack: ['Next.js', 'TailwindCSS', 'Shadcn/UI'],
+//       tags: ['Visual Cards', 'GitHub Contribution Cards']
+//     },
+//   ]
   
-  export const githubProjects: Array<ProjectItemType> = [
-    {
-      name: 'Devtoolset',
-      description: 'Open-source & database-free developer tools navigator / 开源无数据库配置的开发者工具导航站',
-      link: { href: 'github.com/iAmCorey/devtoolset', label: 'Devtoolset' },
-      gitStars: 203,
-      gitForks: 67
-    },
-    {
-      name: 'Corey Chiu Portfolio Template',
-      description:
-        'portfolio template by corey chiu',
-      link: { href: 'github.com/iAmCorey/coreychiu-portfolio-template', label: 'Corey Chiu Portfolio Template' },
-      gitStars: 229,
-      gitForks: 30
-    },
-  ]
+//   export const githubProjects: Array<ProjectItemType> = [
+//     {
+//       name: 'Devtoolset',
+//       description: 'Open-source & database-free developer tools navigator / 开源无数据库配置的开发者工具导航站',
+//       link: { href: 'github.com/iAmCorey/devtoolset', label: 'Devtoolset' },
+//       gitStars: 203,
+//       gitForks: 67
+//     },
+//     {
+//       name: 'Corey Chiu Portfolio Template',
+//       description:
+//         'portfolio template by corey chiu',
+//       link: { href: 'github.com/iAmCorey/coreychiu-portfolio-template', label: 'Corey Chiu Portfolio Template' },
+//       gitStars: 229,
+//       gitForks: 30
+//     },
+//   ]
   
