90b39dc962571a73da90c3667ffc4e1e2321663c
xuf <sherlock.xpf@gmail.com>
2026-04-27 17:51:33 +0800

    fix:header hover color

--- app/(main)/NavigationBar.tsx
+++ app/(main)/NavigationBar.tsx
@@ -25,8 +25,8 @@ function NavItem({
         className={clsxm(
           'relative block whitespace-nowrap px-3 py-2 transition',
           isActive
-            ? 'text-lime-600 dark:text-lime-400'
-            : 'hover:text-lime-600 dark:hover:text-lime-400'
+            ? 'text-gray-600 dark:text-gray-400'
+            : 'hover:text-gray-600 dark:hover:text-gray-400'
         )}
       >
         {children}
